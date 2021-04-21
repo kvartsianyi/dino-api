d194f5f2f84f7f170e9eeb1b475ddbb1f6e36ccc
Yura <liparg104@gmail.com>
2021-04-21 23:59:18 +0300

    [ADD] added eslint, morgan, default config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan';
+import * as config from 'config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(morgan('dev'));
+
+  await app.listen(config.get('port'));
 }
 bootstrap();
