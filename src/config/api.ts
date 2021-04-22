1e54c2c6e65f3a0c842a8031fd928906a278d2a2
Yura <liparg104@gmail.com>
2021-04-22 21:44:58 +0300

    [ADD] added 'development' environment

---
+++ src/config/api.ts
@@ -0,0 +1,5 @@
+import { get } from 'config';
+
+export const API_CONFIG = {
+  PORT: get('api.port'),
+};
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import * as morgan from 'morgan';
-import * as config from 'config';
 
+import { API_CONFIG } from './config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
 
-  await app.listen(config.get('port'));
+  await app.listen(API_CONFIG.PORT);
 }
 bootstrap();
