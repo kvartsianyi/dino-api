e91521cfa28c47215121343cf1e1ded8d6e7fec0
Yura <liparg104@gmail.com>
2021-04-27 00:01:49 +0300

    [ADD] added husky pre-commit

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
 
+  // eslint-disable-next-line no-console
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 }
 bootstrap();
